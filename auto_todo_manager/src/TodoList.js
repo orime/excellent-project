73cb9c7ce99b8dc9ce727b49ed703dec4a4a1010
yangye <yangye_0208@youzan.com>
2021-05-26 18:42:31 +0800

    feat: 生成日报功能

--- auto_todo_manager/src/TodoList.js
+++ auto_todo_manager/src/TodoList.js
@@ -1,4 +1,5 @@
-import { Card, Checkbox, Input, InputNumber, List } from "antd"
+import { Button, Card, Checkbox, Input, InputNumber, List } from "antd"
+import TextArea from "antd/lib/input/TextArea"
 import { useCallback, useEffect, useState } from "react"
 
 const ListItem = List.Item
@@ -7,12 +8,13 @@ const titleText = `今日待办：${new Date().toLocaleDateString()}`
 
 const TodoList = () => {
   const [data, setData] = useState([])
+  const [report, setReport] = useState('')
 
   useEffect(() => {
     setData([
-      {id: 1, title: '测试用例审查修改', done: false},
-      {id: 2, title: 'ESLint规则修复', done: false},
-      {id: 3, title: 'SCRM优惠券问题排查', done: false},
+      {id: 1, title: '测试用例审查修改', done: false, process: 50},
+      {id: 2, title: 'ESLint规则修复', done: false, process: 50},
+      {id: 3, title: 'SCRM优惠券问题排查', done: false, process: 50},
     ])
   }, [])
 
@@ -21,7 +23,25 @@ const TodoList = () => {
     const target = data[targetIndex]
     data.splice(targetIndex, 1, {...target, done: !target.done})
     setData([...data])
+    handleGeneReport()
   }, [data])
+
+  const handleGeneReport = useCallback(() => {
+    let res = `${titleText}\n`
+    data.forEach((todo, index) => {
+      res += `\n${index+1}. ${todo.title} - 进度${todo.process}%`
+    })
+    res += `\n\n总结：充实的一天`
+    setReport(res)
+  }, [data])
+
+  const handleProcess = useCallback((value, id) => {
+    const targetIndex = data.findIndex(todo => todo.id === id)
+    const target = data[targetIndex]
+    data.splice(targetIndex, 1, {...target, process: value})
+    setData([...data])
+    handleGeneReport()
+  }, [data, handleGeneReport])
 
   return (<Card title={titleText}>
     <List
@@ -31,10 +51,12 @@ const TodoList = () => {
           <Checkbox onChange={() => handleCheck(item.id)}></Checkbox>&nbsp;&nbsp;
           {item.title}
           { item.done ? '100%' : '0%' }
-          <div style={{float: 'right'}}>完成度 <InputNumber min={0} max={100} formatter={value => `${value}%`} /></div>
+          <div style={{float: 'right'}}>进度 <InputNumber value={item.process} min={0} max={100} formatter={value => `${value}%`} onChange={(value) => handleProcess(value, item.id)} /></div>
         </ListItem>
       )}
     />
+    <Button onClick={handleGeneReport}>生成日报</Button>
+    <TextArea value={report} rows={10}></TextArea>
   </Card>)
 }
 
